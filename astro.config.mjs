cede26e7b9ad1c573d2b2455e5d10e124297928d
mudgrunt <fullertonkyler@gmail.com>
2023-08-23 16:29:19 -0600

    initial build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
